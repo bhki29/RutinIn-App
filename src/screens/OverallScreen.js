daeba87a77483de32bb9e664d419490052afc763
Aji Ahmad Baehaki <ajiahmadbaehaki20@gmail.com>
2026-09-04 20:39:02 +0700

    Project initialization and initial setup of the application structure

---
+++ src/screens/OverallScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function OverallScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Overall Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+});
---
+++ src/screens/TodayScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TodayScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Today Screen — belum diimplementasikan</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+});
