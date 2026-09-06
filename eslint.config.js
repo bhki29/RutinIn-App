ff03c0a6de51f07a3f71731b8a68b23ad5b70e39
Aji Ahmad Baehaki <ajiahmadbaehaki20@gmail.com>
2026-09-06 16:04:45 +0700

    chore: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,35 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require("eslint-config-expo/flat");
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-module.exports = defineConfig([
-  expoConfig,
+export default [
   {
-    ignores: ["dist/*"],
-  }
-]);
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        __DEV__: "readonly",
+        fetch: "readonly",
+        FormData: "readonly",
+        navigator: "readonly",
+        XMLHttpRequest: "readonly",
+        requestAnimationFrame: "readonly",
+        cancelAnimationFrame: "readonly",
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
